ee2723db2961c14ce7a26e73c431fdb8c9db268a
sang <days_v@qq.com>
2016-03-31 01:45:14 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import "babel-polyfill"
-
 function mixin(...mixins) {
     class Mix {}
-
     for (let mixin of mixins) {
         copyProperties(Mix, mixin)
         copyProperties(Mix.prototype, mixin.prototype)
     }
-
     return Mix
 }
-
 function copyProperties(target, source) {
     for (let key of Reflect.ownKeys(source)) {
         if (key !== "constructor" && key !== "prototype" && key !== "name") {
@@ -19,5 +15,4 @@ function copyProperties(target, source) {
         }
     }
 }
-
 export default mixin
